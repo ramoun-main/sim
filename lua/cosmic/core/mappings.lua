78dbd3749bc75af9b6cc6fdfd100b5465a4b8430
OmarRamoun <omar@nurserystory.co.uk>
2023-06-09 21:11:07 +0300

    feat(keymaps): custom keymaps

--- lua/cosmic/core/mappings.lua
+++ lua/cosmic/core/mappings.lua
@@ -33,3 +33,51 @@ map('n', '<C-Up>', ':resize -2<CR>', { desc = 'Resize -2' })
 map('n', '<C-Down>', ':resize +2<CR>', { desc = 'Resize +2' })
 map('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Vertical Resize -2' })
 map('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Vertical Resize +2' })
+
+-- my custom remaps (entrirely)
+
+-- use jj/jk to exit insert mode
+map('i', 'jj', '<ESC>')
+map('i', 'jk', '<ESC>')
+
+-- clear search highlights
+map('n', '<leader>nh', ':nohl<CR>')
+
+-- delete single character without copying into register
+map('n', 'x', '"_x')
+
+-- increment/decrement numbers
+map('n', '<leader>+', '<C-a>') -- increment
+map('n', '<leader>-', '<C-x>') -- decrement
+
+-- window management
+map('n', '<leader>sv', '<C-w>v') -- split window vertically
+map('n', '<leader>sh', '<C-w>s') -- split window horizontally
+map('n', '<leader>se', '<C-w>=') -- make split windows equal width & height
+map('n', '<leader>sx', ':close<CR>') -- close current split window
+
+map('n', '<leader>to', ':tabnew<CR>') -- open new tab
+map('n', '<leader>tx', ':tabclose<CR>') -- close current tab
+map('n', '<leader>tn', ':tabn<CR>') --  go to next tab
+map('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
+
+-- sidebar
+
+map('n', '<leader>e', ':NvimTreeToggle<CR>') -- toggle file explorer
+
+map('n', '<C-j>', '<C-w>j', { desc = 'Move to below split' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move to above split' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move to right split' })
+map('n', '<C-Up>', '<cmd>resize -2<CR>', { desc = 'Resize split up' })
+map('n', '<C-Down>', '<cmd>resize +2<CR>', { desc = 'Resize split down' })
+map('n', '<C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Resize split left' })
+map('n', '<C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Resize split right' })
+  (
+  --[[ maps.n["<leader>w"] = { "<cmd>w<cr>", desc = "Save" } ]]
+[[ maps.n["<leader>q"] = { "<cmd>confirm q<cr>", desc = "Quit" } ]])
+  ([[ maps.n["<leader>n"] = { "<cmd>enew<cr>", desc = "New File" } ]])
+--[[ maps.n["gx"] = { utils.system_open, desc = "Open the file under cursor with system app" } ]]
+--[[ maps.n["<C-s>"] = { "<cmd>w!<cr>", desc = "Force write" } ]]
+--[[ maps.n["<C-q>"] = { "<cmd>q!<cr>", desc = "Force quit" } ]]
+--[[ maps.n["|"] = { "<cmd>vsplit<cr>", desc = "Vertical Split" } ]]
+--[[ maps.n["\\"] = { "<cmd>split<cr>", desc = "Horizontal Split" } ]]
