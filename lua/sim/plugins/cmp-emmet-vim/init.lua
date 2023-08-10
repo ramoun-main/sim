1d870448252451816f01e170d3cee199c4b7fb48
Omar Ramoun <contact@omarramoun.com>
2023-08-10 04:51:48 +0300

    fix(vim-emmet): blocking auto complete from workign

--- lua/sim/plugins/cmp-emmet-vim/init.lua
+++ lua/sim/plugins/cmp-emmet-vim/init.lua
@@ -2,17 +2,19 @@ local editor_name = 'sim'
 
 local map = require(string.format('%s.utils', editor_name)).map
 
-return {
-  'dcampos/cmp-emmet-vim',
-  dependencies = { 'mattn/emmet-vim' },
-  event = 'BufEnter',
-  config = function()
-    require('cmp').setup({
-      sources = {
-        {
-          name = 'emmet_vim',
-        },
-      },
-    })
-  end,
-}
+--[[ return { ]]
+--[[   'dcampos/cmp-emmet-vim', ]]
+--[[   dependencies = { 'mattn/emmet-vim' }, ]]
+--[[   event = 'BufEnter', ]]
+--[[   config = function() ]]
+--[[     require('cmp').setup({ ]]
+--[[       sources = { ]]
+--[[         { ]]
+--[[           name = 'emmet_vim', ]]
+--[[         }, ]]
+--[[       }, ]]
+--[[     }) ]]
+--[[   end, ]]
+--[[ } ]]
+
+return {}
