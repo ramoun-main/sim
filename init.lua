474426326fa39bb2df4a95ed5cef4d5c72fb11c4
Omar Ramoun <contact@omarramoun.com>
2023-08-13 23:35:22 +0300

    refactor(init): use the new global EditorName

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ require('editor-name')
 
 -- check if vim version is capable of running the Sim Editor
 if vim.fn.has('nvim-0.8') == 0 then
-  error(string.format('Need Neovim v0.8+ in order to run %s', 'sim'))
+  error(string.format('Need Neovim v0.8+ in order to run %s', EditorName))
 end
 
--- import name of editor
-local ok, err = pcall(require, 'sim')
+-- load the editor
+local ok, err = pcall(require, EditorName)
 
 if not ok then
   error(('Error loading core...\n\n%s'):format(err))
