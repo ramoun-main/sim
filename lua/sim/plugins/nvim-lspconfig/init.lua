764d4e31c94d1b1deaf728fad8e88908f5b2a275
Omar Ramoun <contact@omarramoun.com>
2023-08-14 01:43:58 +0300

    refactor(nvim-lspconfig): move configs to sperate files

--- lua/sim/plugins/nvim-lspconfig/init.lua
+++ lua/sim/plugins/nvim-lspconfig/init.lua
@@ -1,9 +1,6 @@
-local editor_name = 'sim'
-
-local user_config = require(string.format('%s.core.user', editor_name))
+local user_config = require(string.format('%s.core.user', EditorName))
 
 return {
   'neovim/nvim-lspconfig',
   enabled = not vim.tbl_contains(user_config.disable_builtin_plugins, 'nvim-lspconfig'),
 }
-
