10a904684923a7c3c0254e0e19be9e5c91a1b199
Omar Ramoun <contact@omarramoun.com>
2023-08-13 23:32:07 +0300

    refactor(root): convert editor name to global variable

--- editor-name.lua
+++ editor-name.lua
@@ -1,5 +1,7 @@
 -- NOTE: Make Sure that EDITOR_NAME matches the name of the child directory inside of the lua directory
 -- `lua/${EDITOR_NAME}`
 
+EditorName = 'sim'
+
 local EDITOR_NAME = 'sim'
 return EDITOR_NAME
--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+--[[ import global editor name ]]
+require('editor-name')
+
 -- check if vim version is capable of running the Sim Editor
 if vim.fn.has('nvim-0.8') == 0 then
   error(string.format('Need Neovim v0.8+ in order to run %s', 'sim'))
@@ -9,4 +12,3 @@ local ok, err = pcall(require, 'sim')
 if not ok then
   error(('Error loading core...\n\n%s'):format(err))
 end
-
