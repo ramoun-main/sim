35a156c58a375ff7ee9f06d2bf77369c83706eb3
Omar Ramoun <contact@omarramoun.com>
2023-08-13 23:52:14 +0300

    feat(auto-pairs): move opts to a sperate file

--- lua/sim/plugins/auto-pairs/init.lua
+++ lua/sim/plugins/auto-pairs/init.lua
@@ -1,24 +1,11 @@
 local user_config = require(string.format('%s.core.user', EditorName))
 local config = require(string.format('%s.plugins.auto-pairs.config', EditorName))
+local opts = require(string.format('%s.plugins.auto-pairs.opts', EditorName))
 
 return {
   'windwp/nvim-autopairs',
-  config = config,
   event = 'InsertEnter',
-  opts = {
-    check_ts = true,
-    ts_config = { java = false },
-    fast_wrap = {
-      map = '<C-e>',
-      chars = { '{', '[', '(', '"', "'", '<' },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
-      offset = 0,
-      end_key = '$',
-      keys = 'qwertyuiopzxcvbnmasdfghjkl',
-      check_comma = true,
-      highlight = 'PmenuSel',
-      highlight_grey = 'LineNr',
-    },
-  },
-  enabled = not vim.tbl_contains(user_config.disable_builtin_plugins, 'autopairs'),
+  config = config,
+  opts = opts,
+  enabled = not vim.tbl_contains(user_config.disable_builtin_plugins, 'auto-pairs'),
 }
