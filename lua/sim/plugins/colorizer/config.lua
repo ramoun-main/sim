36a06acbdb7f3316588987896246d2565e159615
Omar Ramoun <contact@omarramoun.com>
2023-10-01 08:55:50 +0300

    refactor(plugins): colorizer

--- lua/sim/plugins/colorizer/config.lua
+++ lua/sim/plugins/colorizer/config.lua
@@ -1,8 +1,16 @@
-local user_config = require(string.format('%s.core.user', EditorName))
-local u = require(string.format('%s.utils', EditorName))
+local map = require(string.format('%s.utils', EditorName)).map
 
-local defaults = {}
+--[[ local defaults = { ]]
+-- RRGGBBAA = true, -- #RRGGBBAA hex codes
+-- rgb_fn = true, -- CSS rgb() and rgba() functions
+-- hsl_fn = true, -- CSS hsl() and hsla() functions
+-- css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+-- css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+--[[ } ]]
 
 return function()
-  require('colorizer').setup(u.merge(defaults, user_config.plugins.colorizer or {}))
+  vim.cmd('ColorizerToggle')
+  require('colorizer').setup(nil, { css = true })
+  vim.cmd(':ColorizerAttachToBuffer')
+  map('n', '<leader>ct', ':ColorizerToggle<CR>')
 end
